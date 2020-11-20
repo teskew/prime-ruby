270458fe93813480c89b65f2ef9fa493954011c1
teskew <teskinder1@gmail.com>
2020-11-20 13:00:55 -0600

    commit tree 0298d80a429d392b1143e38899361fe6cf8a66cd

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime? (integer)
+
+    if integer <= 1
+        return false
+    end
+    counter = 2
+    while counter < integer
+        if integer % counter == 0
+        return false
+        end
+        counter = counter + 1
+end
+true
+end 
